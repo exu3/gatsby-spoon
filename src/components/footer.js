79bd906a8a7714e2229fe23187f78512e8223758
Ella <exu6056@gmail.com>
2020-12-19 20:34:49 -0800

    everything else 2

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,19 @@
+import { graphql, useStaticQuery } from 'gatsby'
 import React from 'react'
 
 const Footer = () => {
+    const data = useStaticQuery(graphql`
+      query {
+          site {
+              siteMetadata {
+                  author
+              }
+          }
+      }
+    `)
     return (
         <footer>
-            <p>Created by me, © 2027</p>
+            <p>Created by {data.site.siteMetadata.author}, © 2027</p>
         </footer>
     )
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,33 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { Link, graphql, useStaticQuery } from 'gatsby'
+
+import headerStyles from './header.module.scss'
 
 const Header = () => {
+    const data = useStaticQuery(graphql `
+      query {
+          site {
+              siteMetadata {
+                  title
+              }
+          }
+      }
+    `)
+
     return (
-        <header>
-            <h1>A Person's Website</h1>
+        <header className={headerStyles.header}>
+            <Link className={headerStyles.title} to="/">
+                {data.site.siteMetadata.title}
+            </Link>
             <nav>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/blog">Blog</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/contact">Contact</Link></li>
+                <ul className={headerStyles.navList}>
+                    <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/">Home</Link></li>
+                    
+                    <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/blog">Blog</Link></li>
+                    
+                    <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/about">About</Link></li>
+                    
+                    <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/contact">Contact</Link></li>
                 </ul>
             </nav>
         </header>
